95546dd06a45b2362635537da25da11a7f0cb686
Alin Vlad <alinvlad05@gmail.com>
2020-12-29 14:15:12 +0200

    Initial commit

---
+++ TestSingleton.java
@@ -0,0 +1,14 @@
+public class TestSingleton {
+    public static void main(String args[]) {
+        Database database;
+
+        database = Database.getInstance("products");
+
+        System.out.println("This is the " + database.getName() + " database.");
+
+        database = Database.getInstance("employees");
+
+        System.out.println("This is the " + database.getName() + " database.");
+    }
+
+}
